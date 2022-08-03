38aa31ea0605115da7bb7bb25398cb58384f71db
Zhangir Azerbayev <zazerbayev@gmail.com>
2022-08-02 20:46:22 -0400

    working but slow version of make data

--- make_data.py
+++ make_data.py
@@ -14,17 +14,21 @@ def make_proof_dict(path):
     pf_list = proofs.split("Proof of ")
 
     pf_dict = {re.search("\".*?\"", x).group()[1:-1]: 
-            x[x.index("\n"):].rstrip("\n") for x in pf_list[1:-1]}
+            x[x.index("\n"):].rstrip("\n") for x in pf_list[1:]}
 
     return pf_dict
 
 def replace_proof_of_key(key, new_proof, set_mm): 
+    #print("PROOF: ", new_proof)
     match_obj = re.search(key + " ", set_mm)
     sep = match_obj.span()[1]+1 
 
     rest = set_mm[sep:]
-    rest = re.sub(r"\$=.*?\$\.", "$=\n\t\t" + new_proof.replace("\n", "\n\t\t") + "$.\n", 
+    pattern = re.compile(r"\$=.*?\$\.", re.DOTALL)
+    #print("GROUP: ", re.search(pattern, rest).group())
+    rest = re.sub(pattern, "$=" + " "*6 + new_proof.replace("\n", "\n" + " "*6) + " $.", 
             rest, count=1)
+    #print("REST:\n", rest[:1000])
 
     set_mm = set_mm[:sep] + rest 
 
@@ -36,12 +40,13 @@ def main():
 
     with open("metamath-0.198/set.mm") as f: 
         set_mm = f.read()
-
-    set_mm = set_mm[:1000]
-
+    
+    # I know this for loop is horrible but I only need to run it once 
     for key in tqdm(proof_dict): 
         set_mm = replace_proof_of_key(key, proof_dict[key], set_mm)
 
+    with open("human-readable.mm", "w") as f: 
+        f.write(set_mm)
 
 if __name__=="__main__": 
     main()
